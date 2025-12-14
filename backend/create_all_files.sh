ca9bd607b3f5fcd12593473324b60d87ca7ee868
YTKA <Vzhernyakov8896955@gmail.com>
2025-12-14 15:47:06 +0300

    Update1

---
+++ backend/create_all_files.sh
@@ -0,0 +1,4 @@
+echo
+mkdir -p trading-bot-platform/{backend/app/{bot,api,models},backend/tests,frontend/{public,src/{components,stores,utils}},infrastructure/{nginx,certbot},scripts,docs,.github/workflows}
+cd trading-bot-platform
+
